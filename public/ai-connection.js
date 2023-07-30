4e80dcab1371fa2f2ab5b8538630b86f07db70aa
SamRyong <zackinthebox@khu.ac.kr>
2023-07-30 19:06:08 +0900

    ice 서버 5개로 증설

--- public/ai-connection.js
+++ public/ai-connection.js
@@ -14,7 +14,10 @@ const statusDiv = document.getElementById('status');
 const configuration = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
-    { urls: 'stun:stun1.l.google.com:19302' }
+    { urls: 'stun:stun1.l.google.com:19302' },
+    { urls: 'stun:stun2.l.google.com:19302' },
+    { urls: 'stun:stun3.l.google.com:19302' },
+    { urls: 'stun:stu4.l.google.com:19302' }
   ]
 };
 const socket = io();

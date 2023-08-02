2e51810c5854f2c0f2c8728dce3a29f1214c491d
SamRyong <zackinthebox@khu.ac.kr>
2023-08-02 15:01:51 +0900

    asdasdf

--- routers/webchatrout.js
+++ routers/webchatrout.js
@@ -3,6 +3,8 @@ const jwt = require("jsonwebtoken");
 const router = express.Router();
 const path = require('path');
 const cookieSession = require("cookie-session");
+const db = require("../Login/app/models/index");
+const User = db.user;
 
 router.use(
   cookieSession({
@@ -12,17 +14,22 @@ router.use(
   })
 );
 
-router.get('/chat',(req,res) => {
-        jwt.verify(req.session.token, "bezkoder-secret-key", (err, decoded) => {
-                if (err) {
-        return res.status(401).send({
-          message: "Unauthorized!",
-        });
-      }
-      req.userId = decoded.id;
-    console.log(req.userId);
-    console.log(req.session.token);
-    res.sendFile(path.resolve(__dirname, "../public/index_wow.html"));
+router.get('/chat',async (req,res) => {
+    jwt.verify(req.session.token, "bezkoder-secret-key", async (err, decoded) => {
+        if (err) {
+            return res.status(401).send({
+                message: "Unauthorized!",
+            });
+        }
+        else{
+
+            req.userId = decoded.id;
+            const user = await User.findById(req.userId);
+            const code = user.code;
+            console.log(code);
+            res.sendFile(path.resolve(__dirname, "../public/index_wow.html"));
+        }
+    
 });
 });
 

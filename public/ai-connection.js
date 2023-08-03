b211b430f414bb361c9f388b7c0f5cf8b07faa44
SamRyong <zackinthebox@khu.ac.kr>
2023-08-04 03:28:57 +0900

    test

--- public/ai-connection.js
+++ public/ai-connection.js
@@ -150,19 +150,14 @@ let ready = 0;
         // ICE candidate 받기
         socket.on('ice-candidate', async (candidate) => {
           await peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
-          captureContext.drawImage(webcamStream, 0, 0, captureCanvas.width, captureCanvas.height);
-          imageData = captureCanvas.toDataURL('image/png');
-          socket.emit('connect_ai', imageData);
         });
 
         // ICE candidate 보내기
         peerConnection.onicecandidate = (event) => {
           if (event.candidate) {
-            setTimeout(async () => {
-                await socket.emit('ice-candidate', event.candidate, currentRoom);
-                captureContext.drawImage(webcamStream, 0, 0, captureCanvas.width, captureCanvas.height);
-                imageData = captureCanvas.toDataURL('image/png');
-                socket.emit('connect_ai', imageData);
+            setTimeout( () => {
+                console.log("emit ice");
+                socket.emit('ice-candidate', event.candidate, currentRoom);
               }, 1000);
           }
         };

c91bf1815e64e91649f7723b10e8b32eaa22ebea
SamRyong <zackinthebox@khu.ac.kr>
2023-08-04 00:31:01 +0900

    asdfasdf1

--- public/ai-connection.js
+++ public/ai-connection.js
@@ -128,6 +128,12 @@ let ready = 0;
         socket.on('answer', async (answer) => {
             await peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
             console.log("thisisfirst");
+            peerConnection.onicecandidate = async (event) => {
+              if (event.candidate) {
+                console.log("thisissecond");
+                socket.emit('ice-candidate', event.candidate, currentRoom);
+              }
+            };
         });
 
         // ICE candidate 받기
@@ -147,12 +153,7 @@ let ready = 0;
         socket.emit('offer', peerConnection.localDescription, currentRoom);
 
         // ICE candidate 보내기
-        peerConnection.onicecandidate = async (event) => {
-          if (event.candidate) {
-            console.log("thisissecond");
-            socket.emit('ice-candidate', event.candidate, currentRoom);
-          }
-        };
+        
 
         // 원격 비디오 스트림 받기
         peerConnection.ontrack = async (event) => {
